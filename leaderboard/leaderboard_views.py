c6353846db9103b48777a9dfd1a7bc392fc649d9
mathisdlg <delage.mathis.1@gmail.com>
2025-07-22 16:52:51 +0200

    Add the edition of leaderboard

--- leaderboard/leaderboard_views.py
+++ leaderboard/leaderboard_views.py
@@ -12,7 +12,6 @@ def leaderboard_list(request):
     if not request.user.is_authenticated:
         leaderboards = Leaderboard.objects.filter(public=True).order_by('-id')
     else:
-        # Get all leaderboards that the user has access to
         leaderboards = Leaderboard.objects.filter(
             creator=request.user
         ) | Leaderboard.objects.filter(shared_with_users=request.user) | Leaderboard.objects.filter(public=True)
@@ -67,3 +66,45 @@ def create_leaderboard(request):
         return render(request, 'leaderboard/leaderboard_detail.html', {'leaderboard': leaderboard})
 
     return render(request, 'leaderboard/create_leaderboard.html', {'users': users})
+
+@login_required
+def edit_leaderboard(request, leaderboard_id):
+    try:
+        leaderboard = Leaderboard.objects.get(id=leaderboard_id, creator=request.user)
+    except Leaderboard.DoesNotExist:
+        return render(request, 'error/404.html', status=404, context={'message': 'Leaderboard not found or you do not have permission to edit it'})
+
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        description = request.POST.get('description', None)
+        public = request.POST.get('public', 'false') == 'true'
+        shared_with = request.POST.get('shared_with_users', "").split(",")
+        if shared_with == [''] or not shared_with:
+            shared_with = []
+        
+        if not name:
+            return render(request, 'error/invalid_input.html', {'message': 'Name is required'})
+        
+        leaderboard.name = name
+        leaderboard.description = description
+        leaderboard.public = public
+        leaderboard.shared_with_users.set(shared_with)
+        leaderboard.save()
+        
+        return redirect('leaderboard_detail', leaderboard_id=leaderboard.id)
+
+    users = User.objects.all().exclude(id=request.user.id)
+    return render(request, 'leaderboard/edit_leaderboard.html', {'leaderboard': leaderboard, 'users': users})
+
+@login_required
+def delete_leaderboard(request, leaderboard_id):
+    try:
+        leaderboard = Leaderboard.objects.get(id=leaderboard_id, creator=request.user)
+    except Leaderboard.DoesNotExist:
+        return render(request, 'error/404.html', status=404, context={'message': 'Leaderboard not found or you do not have permission to delete it'})
+
+    if request.method == 'POST':
+        leaderboard.delete()
+        return redirect('leaderboards_list')
+
+    return render(request, 'leaderboard/delete_leaderboard.html', {'leaderboard': leaderboard})
