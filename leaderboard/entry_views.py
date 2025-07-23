fa6be5db0a32f671ebce3aa92601c70ddf57495d
mathisdlg <delage.mathis.1@gmail.com>
2025-07-23 10:03:48 +0200

    Add delete entry

--- leaderboard/entry_views.py
+++ leaderboard/entry_views.py
@@ -8,16 +8,33 @@ def add_entry(request, leaderboard_id):
     leaderboard = Leaderboard.objects.get(id=leaderboard_id)
     if not leaderboard.have_access(request.user):
         return render(request, 'error/auth_req.html')
+    options = RealiseWith.objects.filter(belongs_to=leaderboard)
     
     if request.method == "POST":
-        name = request.POST.get('name')
         score = request.POST.get('value')
-        option_ids = request.POST.getlist('options', [])
+        option_ids = request.POST.get('realise_with_options', '').split(',')
+        if option_ids == ['']:
+            option_ids = []
 
-        if name:
-            entry = LeaderboardEntry(name=name, score=score, realise_with=RealiseWith.objects.filter(id__in=option_ids))
+        if score:
+            entry = LeaderboardEntry(user=request.user, score=score)
             entry.save()
+            entry.realise_with.set(RealiseWith.objects.filter(id__in=option_ids))
             leaderboard.entries.add(entry)
             return redirect('leaderboard_detail', leaderboard_id=leaderboard.id)
 
-    return render(request, "leaderboard/entries/add_entry.html", {"leaderboard": leaderboard})
+    return render(request, "leaderboard/entries/add_entry.html", {"leaderboard": leaderboard, "options": options})
+
+@login_required
+def delete_entry(request, leaderboard_id, entry_id):
+    try:
+        leaderboard = Leaderboard.objects.get(id=leaderboard_id, creator=request.user)
+        entry = LeaderboardEntry.objects.get(id=entry_id, user=request.user)
+    except (Leaderboard.DoesNotExist, LeaderboardEntry.DoesNotExist):
+        return render(request, 'error/404.html', status=404, context={'message': 'Leaderboard or entry not found or you do not have permission to delete it'})
+
+    if request.method == 'POST':
+        entry.delete()
+        return redirect('leaderboard_detail', leaderboard_id=leaderboard.id)
+
+    return render(request, 'leaderboard/entries/delete_entry.html', {'entry': entry, 'leaderboard': leaderboard})
