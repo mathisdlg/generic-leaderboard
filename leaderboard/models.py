fd7574881a3c8395146f921ad7e2118031d9457d
Yann Champeau <yann.champeau@michelin.com>
2025-07-29 14:23:53 +0200

    Reverted datetime to varchar data type.

--- leaderboard/models.py
+++ leaderboard/models.py
@@ -4,7 +4,8 @@ from django.contrib.auth.models import User
 # Create your models here.
 class LeaderboardEntry(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    score = models.DateTimeField()
+    score = models.CharField(max_length=100)
+    # score = models.DateTimeField()
     realise_with = models.ManyToManyField('RealiseWith', blank=True)
 
     def __str__(self):
